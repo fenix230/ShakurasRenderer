7e29d5f6e6d75f5d39e209d05ad1d554d30f187a
fenix230 <fenix230@126.com>
2016-08-24 10:35:38 +0800

    123

--- Code/SoftRenderer/SoftColorFormat.h
+++ Code/SoftRenderer/SoftColorFormat.h
@@ -5,6 +5,8 @@
 SHAKURAS_BEGIN;
 
 
+//uint32_t - b, g, r
+//Vector3f - r, g, b
 class ColorFormatU32F3 {
 public:
 	typedef uint32_t data_t;
@@ -13,18 +15,18 @@ public:
 	static inline Vector3f scalar(uint32_t d) {
 		Vector3f v;
 		const unsigned char* pu = (const unsigned char*)&d;
-		v.x = pu[0] / 255.0f;
+		v.x = pu[2] / 255.0f;
 		v.y = pu[1] / 255.0f;
-		v.z = pu[2] / 255.0f;
+		v.z = pu[0] / 255.0f;
 		return v;
 	}
 
 	static inline uint32_t data(const Vector3f& v) {
 		uint32_t u = 0;
 		unsigned char* pu = (unsigned char*)&u;
-		pu[0] = (int)(v.x * 255);
+		pu[2] = (int)(v.x * 255);
 		pu[1] = (int)(v.y * 255);
-		pu[2] = (int)(v.z * 255);
+		pu[0] = (int)(v.z * 255);
 		pu[3] = 255;
 		return u;
 	}
